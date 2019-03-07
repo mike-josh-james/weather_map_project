ec46e3ead9d0265354c713fef4b08b885a605c4f
Jlampkins <Jlampkins42@gmail.com>
2019-03-07 14:52:56 -0600

    funcationality for map and weather are working

--- weather_map.js
+++ weather_map.js
@@ -1,7 +1,137 @@
 "use strict";
 $(document).ready(function(){
 
-    var url = "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + darkSkyToken + "/37.8267,-122.4233";
-    $.get(url)
+    var lat = "29.4241";
+    var long = "-98.4936";
+    var url = "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + darkSkyToken + "/" + lat + "," + long;
 
-}) ;
+
+    $.get(url).done(function(weather){
+        console.log(weather);
+            getWeather(weather);
+        });
+
+function returnIcon(icon){
+    for(var i = 0; i<=weatherObj.length; i++){
+            if(icon === weatherObj[i].condition){
+                return "<img src=" + weatherObj[i].url + ">";
+            }
+        }
+    }
+
+    $("#search-btn").on("click", function(event){
+        lat = $("#input-lat").val();
+        long = $("#input-long").val();
+        url = "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + darkSkyToken + "/" + lat + "," + long;
+        $.get(url).done(function(weather){
+            console.log(weather);
+            getWeather(weather);
+        });
+    });
+
+function getWeather(weather) {
+    var html ="";
+    for(var i = 0; i <= 2; i++) {
+        html += "<div>";
+        html += weather.daily.data[i].apparentTemperatureHigh + "/";
+        html += weather.daily.data[i].apparentTemperatureLow + "<br>" + returnIcon(weather.daily.data[i].icon) + "<br>";
+        html += weather.daily.data[i].summary + "<br>";
+        html += weather.daily.data[i].humidity + "<br>";
+        html += weather.daily.data[i].windSpeed + "<br>";
+        html += weather.daily.data[i].pressure;
+        html += "</div>";
+    }
+    $("#weather-layout").html(html)
+
+}
+
+var weatherObj = [ {
+        condition: "clear-day",
+        url: "icon/Sun.svg"
+                },
+    {
+        condition: "clear-night",
+            url: "icon/Moon.svg"
+                },
+    {
+        condition: "rain",
+            url: "icon/Umbrella.svg"
+                },
+    {
+        condition: "snow",
+            url: "icon/Snowflake.svg"
+                },
+    {
+        condition: "sleet",
+            url: "icon/Cloud-Hail-Alt.svg"
+                },
+    {
+        condition: "wind",
+            url: "icon/Wind.svg"
+                },
+    {
+        condition: "fog",
+            url: "icon/Cloud-Fog.svg"
+                },
+    {
+        condition: "cloudy",
+            url: "icon/Cloud.svg"
+                },
+    {
+        condition: "partly-cloudy-day",
+            url: "icon/Cloud-Sun.svg"
+                },
+    {
+        condition:"partly-cloudy-night",
+            url: "icon/Cloud-Moon.svg"
+                },
+
+]
+
+
+    /** MAP STUFF **/
+
+    mapboxgl.accessToken = mapboxToken;
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/satellite-streets-v9',
+        zoom: 9,
+        center: [-98.4936, 29.4241]
+    });
+
+    function onDragEnd(){
+        lat = marker.getLngLat().lat;
+        long = marker.getLngLat().lng;
+        url = "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + darkSkyToken + "/" + lat + "," + long;
+        $.get(url).done(function(weather){
+            console.log(weather);
+            getWeather(weather);
+        });
+
+    }
+    var marker = new mapboxgl.Marker();
+    marker.setLngLat([-98.4936, 29.4241]);
+    marker.addTo(map);
+    marker.setDraggable(true);
+    marker.on("dragend", onDragEnd);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
